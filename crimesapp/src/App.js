18d6e112b154f30d822e0218c3453119357d8f08
Marcos Santos <marcos.santos@imed.edu.br>
2021-10-26 21:21:56 -0300

    Ajuste de Services e Variáveis do Firebase

--- crimesapp/src/App.js
+++ crimesapp/src/App.js
@@ -1,10 +1,95 @@
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import React, { useState, useLayoutEffect } from 'react'
+import { storageSave, storageRemove, storageGet } from "./services/Storage"
 
 function App() {
+
+  const [lembreme, setLembreme] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  useLayoutEffect(() => {
+    let emailStorage = storageGet("email")
+    let passwordStorage = storageGet("password")
+    if (emailStorage) {
+      setEmail(emailStorage)
+      setPassword(passwordStorage)
+      setLembreme(true)
+    }
+
+  }, [])
+
+  const handleLembreme = (e) => {
+    setLembreme(e.target.checked)
+
+    if (e.target.checked == true) {
+      storageSave("email", email)
+      storageSave("password", password)
+    } else {
+      storageRemove("email")
+      storageRemove("password")
+    }
+  }
+
+
   return (
-    <div>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    </div>
+    <Grid container spacing={1}>
+      <Grid item xs={3}></Grid>
+      <Grid item xs={6}>
+        <TextField
+          type="email"
+          id="outlined-basic"
+          label="E-mail"
+          variant="outlined"
+          fullWidth
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </Grid>
+      <Grid item xs={3}></Grid>
+      <Grid item xs={3}></Grid>
+      <Grid item xs={6}>
+        <TextField
+          type="password"
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          fullWidth
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+
+        />
+      </Grid>
+      <Grid item xs={3}></Grid>
+      <Grid item xs={3}></Grid>
+      <Grid item xs={6}>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={lembreme}
+                onChange={handleLembreme} />}
+            label="Lembre-me"
+          />
+        </FormGroup>
+        <Button variant="contained" size="small">
+          Login
+        </Button>
+
+      </Grid>
+      <Grid item xs={3}></Grid>
+
+
+
+
+
+    </Grid>
+
   );
 }
 
---
+++ crimesapp/src/services/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrPU5-oXCVw2jMMlweWcnQl_jrLlTcups",
+  authDomain: "mapacrimes.firebaseapp.com",
+  projectId: "mapacrimes",
+  storageBucket: "mapacrimes.appspot.com",
+  messagingSenderId: "860403545620",
+  appId: "1:860403545620:web:543b317244114ff7641901"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
